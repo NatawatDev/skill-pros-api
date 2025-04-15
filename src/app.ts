44214ab3c69c21561ac20766114b9e52d68ccafb
NatawatDev <natwat.codealong@gmail.com>
2025-04-15 16:50:19 +0700

    add:swagger config

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import swaggerUi from 'swagger-ui-express'
+import { createSwaggerSpec } from '@/config/swagger'
 
 dotenv.config()
 
@@ -12,5 +14,6 @@ app.use(helmet())
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(createSwaggerSpec()))
 
 export default app
---
+++ src/config/swagger.ts
@@ -0,0 +1,25 @@
+const swaggerJSDoc = require('swagger-jsdoc')
+
+export const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'SkillPros API',
+      version: '1.0.0',
+      description: 'API documentation for SkillPros admin system',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: ['src/modules/**/*.ts'], 
+}
+
+export const createSwaggerSpec = () => swaggerJSDoc(swaggerOptions)
